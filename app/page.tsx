0dc30dfa3f09dc020ad031c9cb9db246b41bc169
devkiloton <devkiloton@gmail.com>
2023-12-01 22:18:57 -0300

    Updating home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import animations from "@/animations/animations.module.css";
+import FormNewsletter from "@/components/home/FormNewsletter";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-between items-center p-24 py-20">
-      <div className={animations.fade_up + " flex self-start flex-col gap-4"}>
-        <span className="text-7xl font-bold max-w-3xl self-start">
-          Você controlando seus investimentos
-        </span>
-        <span className="self-start text-lg max-w-2xl">
-          Com a Foxbat você tem acesso a uma plataforma completa para gerenciar
-          seus investimentos, com dados em tempo real e de forma simples e
-          intuitiva.
-        </span>
+    <main className="flex min-h-screen justify-between items-center gap-4 p-24 py-20">
+      <div className="self-start flex flex-col gap-20">
+        <div className={animations.fade_up + " flex  flex-col gap-4"}>
+          <span className="text-6xl font-bold max-w-3xl self-start">
+            Você controlando seus investimentos
+          </span>
+          <span className="self-start text-lg max-w-2xl">
+            Com a Foxbat você tem acesso a uma plataforma completa para
+            gerenciar seus investimentos, com dados em tempo real e de forma
+            simples e intuitiva.
+          </span>
+        </div>
+        <div className={animations.move_right_fast}>
+          <FormNewsletter />
+        </div>
       </div>
       <div className="relative">
-        <div className={animations.move_left_fast + " mockup-phone"}>
+        <div className={animations.move_left_fast + " mockup-phone shadow-2xl"}>
           <div className="camera"></div>
           <div className="display">
             <div className="artboard artboard-demo h-[497px] w-[280px]">
@@ -26,7 +32,7 @@ export default function Home() {
         <div
           className={
             animations.move_up_fast +
-            " mockup-browser border bg-base-100 absolute h-1/2 w-96 left-[-250px] bottom-0"
+            " mockup-browser border bg-base-100 absolute h-1/2 w-96 left-[-250px] bottom-5 shadow-2xl"
           }
         >
           <div className="mockup-browser-toolbar">
