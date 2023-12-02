f1ba8f3eea898851929037bf15f4c9f381cfa818
devkiloton <devkiloton@gmail.com>
2023-12-01 22:19:25 -0300

    Creating a container

---
+++ components/shared/ContainerElement.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ContainerElement({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl border">
+      <div className="card-body">{children}</div>
+    </div>
+  );
+}
