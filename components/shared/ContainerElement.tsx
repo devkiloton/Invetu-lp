f0786cfe8e518f80a55f54fe856bc0b0f53d9134
devkiloton <devkiloton@gmail.com>
2023-12-02 21:55:27 -0300

    Updating ContainerElement width props

--- components/shared/ContainerElement.tsx
+++ components/shared/ContainerElement.tsx
@@ -6,7 +6,7 @@ export default function ContainerElement({
   children: React.ReactNode;
 }) {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl border">
+    <div className="card w-full max-w-96 bg-base-100 shadow-xl">
       <div className="card-body p-6">{children}</div>
     </div>
   );
