eedddc64ee581f660b38977a9e102cda9bd3132e
devkiloton <devkiloton@gmail.com>
2023-12-02 21:53:56 -0300

    Creating a basic not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="h-[60vh] flex justify-center items-center">
+      <div className="text-center hero-content text-3xl font-bold">
+        <div>
+          <h1>Página não encontrada.</h1>
+          <div className="mt-4">
+            <Link href="/" className="link-primary">
+              Voltar ao início
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
