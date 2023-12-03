c9d4cadaf4f121b6b2c657f696dd10204af016fc
devkiloton <devkiloton@gmail.com>
2023-12-02 23:18:53 -0300

    Creating env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SERVICE_ID: process.env.SERVICE_ID,
+    PUBLIC_KEY: process.env.PUBLIC_KEY,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    TEMPLATE_ID_SUPPORT: process.env.TEMPLATE_ID_SUPPORT,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
